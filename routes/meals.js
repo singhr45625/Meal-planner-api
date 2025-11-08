8697cce8923c26f79ad43ba5a27f32d07e94e2d7
rishu2004-rs <kpuja0969@gmail.com>
2025-11-08 20:56:01 +0530

    Update meals.js

--- routes/meals.js
+++ routes/meals.js
@@ -16,7 +16,7 @@ const router = express.Router();
 
 // ADD THIS LINE - Basic GET /meals route
 router.get('/', protect, getAllMeals); // or getAllMeals if you want all user's meals
-router.get('/', protect, getUserMeals);
+
 // Your existing routes...
 router.post('/', protect, createMeal);
 router.get('/my-meals', protect, getUserMeals);
