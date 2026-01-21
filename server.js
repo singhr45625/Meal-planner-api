e61fc1c24f3350abd841d2e41ad77ffaa4b7302d
rishu2004-rs <kpuja0969@gmail.com>
2026-01-21 20:08:04 +0530

    cc

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const path = require('path'); // ADD THIS LINE
+const path = require('path'); 
 const connectDB = require('./config/database');
 
 // Load env vars
